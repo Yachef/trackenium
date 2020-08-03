4d3e57dd87b32c15afbd4bd78eb6ff2c88b8f0b8
Yachef <yachef.h@gmail.com>
2020-08-03 23:46:04 +0200

    trying to solve bug api firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/database';
 import 'firebase/storage';
  
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyCNa_Kqdu6LQNkRjWYUw0mU-IOjgRbv5Ds",
+  authDomain: "trackenium.firebaseapp.com",
+  databaseURL: 'https://trackenium.firebaseio.com',
+  projectId: 'trackenium',
+  storageBucket: '541828254607',
+  messagingSenderId: '1:541828254607:web:dbb0024a946f45e08bd9fa',
 };
  
 class Firebase {
